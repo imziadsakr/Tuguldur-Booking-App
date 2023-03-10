6f206d58ad84335bf72f2a63613f073a39eeab8d
Ziad Sakr <im.ziad.sakr@gmail.com>
2023-03-11 08:42:18 +0900

    Tugudulr upwork done

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-content>
+        <v-layout>
+          <v-container v-if='showLogin' v-on:login = "showLogin = false">
+            <TitleBarComponent v-on:login="loginFalse" />
+          </v-container>
+          <v-container v-if='!showLogin'>
+            <DatePickerComponent/>
+          </v-container>
+
+        </v-layout>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import DatePickerComponent from './components/DatePickerComponent'
+import TitleBarComponent from './components/TitleBarComponent'
+
+export default {
+  name: 'App',
+  components: {
+    TitleBarComponent,
+    DatePickerComponent
+  },
+  data () {
+    return {
+      showLogin:true,
+      //
+    }
+  },
+  methods : {
+    loginFalse : function name() {
+      //alert('login!');
+      this.showLogin =false
+    }
+  }
+}
+</script>
---
+++ src/components/DatePickerComponent.vue
@@ -0,0 +1,75 @@
+<template>
+<v-container>
+  <v-layout align-center justify-center row wrap>
+    <v-flex xs12 sm6 md4>
+      <v-dialog
+        ref="dialogFromDate"
+        v-model="showFromDate"
+        :return-value.sync="date1"
+        persistent
+        lazy
+        full-width
+        width="290px"
+      >
+        <v-text-field
+          slot="activator"
+          v-model="date1"
+          label="Check-In Date"
+          prepend-icon="event"
+          readonly
+        ></v-text-field>
+        <v-date-picker v-model="date1" scrollable>
+          <v-spacer> </v-spacer>
+          <v-btn flat color="primary" @click="showFromDate = false">Cancel</v-btn>
+          <v-btn flat color="primary" @click="$refs.dialogFromDate.save(date1)">OK</v-btn>
+        </v-date-picker>
+      </v-dialog>
+    </v-flex>
+
+    <v-flex xs12 sm6 md4>
+      <v-dialog
+        ref="dialogToDate"
+        v-model="showToDate"
+        :return-value.sync="date2"
+        persistent
+        lazy
+        full-width
+        width="290px"
+      >
+        <v-text-field
+          slot="activator"
+          v-model="date2"
+          label="Check-Out Date"
+          prepend-icon="event"
+          readonly
+        ></v-text-field>
+        <v-date-picker v-model="date2" scrollable>
+          <v-spacer></v-spacer>
+          <v-btn flat color="primary" @click="showToDate = false">Cancel</v-btn>
+          <v-btn flat color="primary" @click="$refs.dialogToDate.save(date2)">OK</v-btn>
+        </v-date-picker>
+      </v-dialog>
+    </v-flex>
+  </v-layout>
+  {{date1}}   {{date2}}
+
+</v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      date1: new Date().toISOString().substr(0, 10),
+      date2: new Date().toISOString().substr(0, 10),
+
+      menu: false,
+      menu2: false,
+      showFromDate : false,
+      showToDate : false
+    })
+  }
+</script>
+
+<style>
+
+</style>
